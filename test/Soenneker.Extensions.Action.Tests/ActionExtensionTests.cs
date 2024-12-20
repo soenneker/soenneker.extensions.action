f1962b4a64b4a808b01b33de2f485607e318e7e9
Jake Soenneker <jake@soenneker.com>
2024-12-20 10:45:44 -0600

    Tests

---
+++ test/Soenneker.Extensions.Action.Tests/ActionExtensionTests.cs
@@ -0,0 +1,12 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Action.Tests;
+
+public class ActionExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+    }
+}
