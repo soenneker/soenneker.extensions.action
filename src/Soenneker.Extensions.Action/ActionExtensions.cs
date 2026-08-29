a5d31312462c48c9862231ea359b09ae3a634989
soenneker <jake@soenneker.com>
2026-08-29 12:38:55 -0500

    Improve XML documentation

--- src/Soenneker.Extensions.Action/ActionExtensions.cs
+++ src/Soenneker.Extensions.Action/ActionExtensions.cs
@@ -11,6 +11,7 @@ public static class ActionExtensions
     /// <summary>
     /// Equivalent to <code>new Task(action)</code>
     /// </summary>
+    /// <returns>A task equivalent to <code>new Task(action)</code>.</returns>
     public static Task ToTask(this System.Action action)
     {
         return new Task(action);
@@ -19,6 +20,7 @@ public static class ActionExtensions
     /// <summary>
     /// Equivalent to <code>Task.Run(action)</code>
     /// </summary>
+    /// <returns>A task equivalent to <code>Task.Run(action)</code>.</returns>
     public static Task RunAsync(this System.Action action, CancellationToken cancellationToken = default)
     {
         return Task.Run(action, cancellationToken);
@@ -27,6 +29,7 @@ public static class ActionExtensions
     /// <summary>
     /// Creates a new Task, and then creates a new ValueTask from that
     /// </summary>
+    /// <returns>Creates a new Task, and then creates a new ValueTask from that.</returns>
     public static ValueTask ToValueTask(this System.Action action)
     {
         var task = action.ToTask();
